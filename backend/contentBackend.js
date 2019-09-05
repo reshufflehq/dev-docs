a82bc1adbbf9d218b6a85c97e1d6992ab1a64325
Ryland Goldstein <ryland@binaris.com>
2019-09-05 19:52:54 +0000

    split markdown parsing into separate file

--- backend/contentBackend.js
+++ backend/contentBackend.js
@@ -5,22 +5,8 @@ import {
   update,
 } from '@binaris/shift-db';
 
-import unified from 'unified';
-import parse from 'remark-parse';
-import remark2rehype from 'remark-rehype';
-import stringify from 'rehype-stringify';
-import slug from 'rehype-slug';
-import link from 'rehype-autolink-headings';
-import fm from 'front-matter';
-
 import { validateJWT } from './authBackend';
-
-const processor = unified()
-  .use(parse)
-  .use(remark2rehype)
-  .use(slug)
-  .use(link)
-  .use(stringify);
+import { parseMDLocal } from './parseMD';
 
 const contentKey = 'content';
 
@@ -42,38 +28,6 @@ function cleanRoute(someRoute) {
  */
 async function getContent() {
   return await get(contentKey) || {};
-}
-
-/**
- * Given raw markdown, this function does the following
- *
- * 1. Extract frontmatter from markdown body
- * 2. Parse remaining markdown into valid HTML
- * 3. Return all parsed data + initial raw repr
- *
- * The format of the returned object:
- *
- * {
- *   attributes: [key: string]: string; // K/V of extracted frontmatter attributes
- *   body: string; // everything but fronmatter from the original doc
- *   frontmatter: string; // unparsed frontmatter repr from doc
- *   parsed: string; // html repr of the input markdown content
- *   raw: string; // input markdown
- * }
- *
- * @param { string } markdownContent - content to parse
- *
- * @return { object } - the parsed markdown, frontmatter and raw
- */
-async function parseMDLocal(markdownContent) {
-  const fmContent = fm(markdownContent);
-  const { contents } = await processor.process(fmContent.body);
-  fmContent.parsed = contents;
-  return {
-    ...fmContent,
-    parsed: contents,
-    raw: markdownContent
-  };
 }
 
 /**
