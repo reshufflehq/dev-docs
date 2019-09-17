77507a4ed51cf75f14f1f6cd583d815fefa22884
Ryland Goldstein <ryland@binaris.com>
2019-09-17 20:34:26 +0000

    add more backend functionality, rename some

--- backend/contentBackend.js
+++ backend/contentBackend.js
@@ -1,4 +1,4 @@
-import { get, update, Q, find } from '@reshuffle/db';
+import { get, update, Q, find, remove } from '@reshuffle/db';
 
 import { validateJWT } from './authBackend';
 import { parseMDLocal } from './parseMD';
@@ -16,6 +16,12 @@ function cleanRoute(someRoute) {
   return someRoute.replace(/\s+/g, '-').toLowerCase();
 }
 
+function isValidRoute(route, failAction) {
+  if (route === undefined || route === null) {
+    throw new Error(failAction);
+  }
+}
+
 /**
  * Authenticated route which will convert raw markdown
  * into its valid HTML repr. Also returns frontmatter
@@ -30,6 +36,53 @@ function cleanRoute(someRoute) {
 export async function parseMD(jwt, markdownContent) {
   await validateJWT(jwt);
   return parseMDLocal(markdownContent);
+}
+
+/**
+ * Set the sites home route
+ *
+ * @param { string } jwt - token used for identification
+ * @param { string } route - route to set as home
+ *
+ */
+/* @expose */
+export async function setRouteAsHome(jwt, route) {
+  await validateJWT(jwt);
+  isValidRoute(route, 'Cannot set undefined or null route as home');
+  await update('homeRoute', () => route);
+}
+
+
+/**
+ * Delete the post at the specified route
+ *
+ * @param { string } jwt - token used for identification
+ * @param { string } route - which post to delete
+ */
+/* @expose */
+export async function deletePostByRoute(jwt, route) {
+  await validateJWT(jwt);
+  isValidRoute(route, 'Cannot delete undefined or null route');
+  await remove(`${contentPrefix}${route}`);
+}
+
+/**
+ * Disable the post at the specified route
+ *
+ * @param { string } jwt - token used for identification
+ * @param { string } route - which post to disable/enable
+ * @param { boolean } disable - enable or disable this route
+ */
+/* @expose */
+export async function setDisabledPostByRoute(jwt, route, disable) {
+  await validateJWT(jwt);
+  isValidRoute(route, 'Cannot disable undefined or null route');
+  await update(`${contentPrefix}${route}`, (prevContent) => {
+    return {
+      ...prevContent,
+      disabled: disable,
+    };
+  });
 }
 
 /**
@@ -65,7 +118,10 @@ export async function updateContent(jwt, content) {
   let potentialError = undefined;
   try {
     await update(`${contentPrefix}${route}`, (prevContent) => {
-      return parsed;
+      return {
+        disabled: prevContent.disabled,
+        ...parsed,
+      };
     });
   } catch (err) {
     console.error(err);
@@ -83,6 +139,8 @@ async function contentByRoute(route) {
   const content = await get(`${contentPrefix}${cleanRoute(route)}`);
   if (content === undefined) {
     throw new Error(`No content found for route: ${route}`);
+  } else if (content.disabled) {
+    throw new Error(`Content at route: ${route} is disabled`);
   }
   return content;
 }
@@ -116,14 +174,43 @@ export async function loadContentByRoute(route) {
   return parsed;
 }
 
+async function queryMeta() {
+  return {
+    found: await find(Q.filter(Q.key.startsWith(contentPrefix))),
+    homeRoute: await get('homeRoute') || undefined,
+  };
+}
+
 /**
  * Returns the metadata of all content.
  *
  * @return {object} - metadata of content
  */
-// @expose
-export async function getContentMeta() {
-  const found = await find(Q.filter(Q.key.startsWith(contentPrefix)));
+/** @expose */
+export async function getContentMeta(jwt) {
+  await validateJWT(jwt);
+  const { found, homeRoute } = await queryMeta();
+  const foundWithDisabled = found.map(({ value }) => {
+    return { ...value.attributes, disabled: value.disabled };
+  });
+  return {
+    content: foundWithDisabled,
+    homeRoute,
+  };
+}
+
+/**
+ * Returns the metadata of all public content.
+ *
+ * @return {object} - metadata of public content
+ */
+/** @expose */
+export async function getPublicContentMeta() {
+  const { found, homeRoute } = await queryMeta();
+  const onlyPublic = found.filter(({ disabled }) => !disabled);
   // even if this is empty it works
-  return found.map(({ value }) => value.attributes);
+  return {
+    content: onlyPublic.map(({ value }) => value.attributes),
+    homeRoute,
+  };
 }
