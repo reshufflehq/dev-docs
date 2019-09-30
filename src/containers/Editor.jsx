5bb403eebdd385ea7fc0f3e65bc36f483916510e
Ryland Goldstein <ryland@binaris.com>
2019-09-30 16:30:19 +0000

    local changes to the editor

--- src/containers/Editor.jsx
+++ src/containers/Editor.jsx
@@ -34,6 +34,68 @@ export default class Editor extends Component {
     this.backgroundLoadMeta();
   }
 
+  componentDidMount() {
+    HTMLTextAreaElement.prototype.getCaretPosition = function () { //return the caret position of the textarea
+        return this.selectionStart;
+    };
+    HTMLTextAreaElement.prototype.setCaretPosition = function (position) { //change the caret position of the textarea
+        this.selectionStart = position;
+        this.selectionEnd = position;
+        this.focus();
+    };
+    HTMLTextAreaElement.prototype.hasSelection = function () { //if the textarea has selection then return true
+        if (this.selectionStart == this.selectionEnd) {
+            return false;
+        } else {
+            return true;
+        }
+    };
+    HTMLTextAreaElement.prototype.getSelectedText = function () { //return the selection text
+        return this.value.substring(this.selectionStart, this.selectionEnd);
+    };
+    HTMLTextAreaElement.prototype.setSelection = function (start, end) { //change the selection area of the textarea
+        this.selectionStart = start;
+        this.selectionEnd = end;
+        this.focus();
+    };
+
+    var textarea = document.getElementsByTagName('textarea')[0];
+
+    textarea.onkeydown = function(event) {
+
+        //support tab on textarea
+        if (event.keyCode == 9) { //tab was pressed
+            var newCaretPosition;
+            newCaretPosition = textarea.getCaretPosition() + "    ".length;
+            textarea.value = textarea.value.substring(0, textarea.getCaretPosition()) + "    " + textarea.value.substring(textarea.getCaretPosition(), textarea.value.length);
+            textarea.setCaretPosition(newCaretPosition);
+            return false;
+        }
+        if(event.keyCode == 8){ //backspace
+            if (textarea.value.substring(textarea.getCaretPosition() - 4, textarea.getCaretPosition()) == "    ") { //it's a tab space
+                var newCaretPosition;
+                newCaretPosition = textarea.getCaretPosition() - 3;
+                textarea.value = textarea.value.substring(0, textarea.getCaretPosition() - 3) + textarea.value.substring(textarea.getCaretPosition(), textarea.value.length);
+                textarea.setCaretPosition(newCaretPosition);
+            }
+        }
+        if(event.keyCode == 37){ //left arrow
+            var newCaretPosition;
+            if (textarea.value.substring(textarea.getCaretPosition() - 4, textarea.getCaretPosition()) == "    ") { //it's a tab space
+                newCaretPosition = textarea.getCaretPosition() - 3;
+                textarea.setCaretPosition(newCaretPosition);
+            }
+        }
+        if(event.keyCode == 39){ //right arrow
+            var newCaretPosition;
+            if (textarea.value.substring(textarea.getCaretPosition() + 4, textarea.getCaretPosition()) == "    ") { //it's a tab space
+                newCaretPosition = textarea.getCaretPosition() + 3;
+                textarea.setCaretPosition(newCaretPosition);
+            }
+        }
+    }
+  }
+
   setPost = async (route) =>{
     try {
       const res = await getContentByRoute(this.props.userToken, route);
@@ -95,6 +157,11 @@ export default class Editor extends Component {
             alertVariant: 'danger',
           });
         } else {
+          setTimeout(() => {
+            this.setState({
+              showAlert: undefined,
+            });
+          }, 1000);
           this.setState({
             showAlert: 'Post successfully updated!',
             alertVariant: 'info',
