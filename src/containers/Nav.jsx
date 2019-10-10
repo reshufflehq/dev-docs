88e1bed39370de4ebcd9ca173c882f5344d1c967
Ryland Goldstein <ryland@binaris.com>
2019-10-10 22:43:59 +0000

    refactor nav logo to be separate parts

--- src/containers/Nav.jsx
+++ src/containers/Nav.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import '../style/Nav.scss';
 
 // Reshuffle nav logo
-import logo from '../reshuffle.svg';
+import logo from '../reshuffle-logo.svg';
+import miniLogo from '../mini-reshuffle-logo.svg';
+import docsLink from '../docs-link.svg';
+import miniDocsLink from '../mini-docs-link.svg';
 
 // various links to our social media
 const Links = {
@@ -17,10 +21,29 @@ export default function Nav() {
   return (
     <div className='nav-bar'>
       <div className='nav-bar-content'>
-        <div className='nav-bar-title unselectable'>
-          <a href={Links.RESHUFFLE_HOME}>
-            <img src={logo} alt='Reshuffle logo'/>
-          </a>
+        <div className='nav-bar-logo unselectable'>
+          <div className='logo-container'>
+            <a href={Links.RESHUFFLE_HOME}>
+              <img src={logo} alt='Reshuffle logo'/>
+            </a>
+            <div className='nav-bar-doclink'>
+              <Link to='/'>
+                <img src={docsLink} alt='Reshuffle docs logo'/>
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className='nav-bar-mini-logo unselectable'>
+          <div className='logo-container'>
+            <a href={Links.RESHUFFLE_HOME}>
+              <img src={miniLogo} alt='Reshuffle logo'/>
+            </a>
+            <div className='nav-bar-doclink'>
+              <Link to='/'>
+                <img src={miniDocsLink} alt='Reshuffle docs logo'/>
+              </Link>
+            </div>
+          </div>
         </div>
 
         <div className='nav-bar-icons'>
