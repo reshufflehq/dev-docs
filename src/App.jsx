20c4eb27e786394f9dd194ea45447201045c9a36
josephriosio <blankworldwide@gmail.com>
2019-12-17 11:53:45 -0800

    removed not needed imports

--- src/App.jsx
+++ src/App.jsx
@@ -8,27 +8,22 @@ import Auth from './containers/Auth';
 import Editor from './containers/Editor';
 import Admin from './containers/Admin';
 
-import { checkEmail } from '../backend/contentBackend';
-
 class App extends Component {
   static contextType = AuthContext;
 
   render() {
     // If the route is an auth or admin route, display it if user is right REACT_APP_VALID_HOSTED_DOMAIN .
     // Otherwise, defer to the DevSite nested routing
-    if (checkEmail) {
-      return (
-        <Switch>
-          <PrivateRoute exact path='/editor' component={Editor} />
-          <PrivateRoute path='/admin' component={Admin} />
-          <Route path='/auth' render={props => <Auth {...props} />} />
 
-          <Route component={DevSite} props={this.props} />
-        </Switch>
-      );
-    } else {
-      return <Route component={DevSite} props={this.props} />;
-    }
+    return (
+      <Switch>
+        <PrivateRoute exact path='/editor' component={Editor} />
+        <PrivateRoute path='/admin' component={Admin} />
+        <Route path='/auth' render={props => <Auth {...props} />} />
+
+        <Route component={DevSite} props={this.props} />
+      </Switch>
+    );
   }
 }
 
