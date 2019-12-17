334d5c2e3be413454a9c75d27f008972e0112a83
josephriosio <blankworldwide@gmail.com>
2019-12-16 17:25:25 -0800

    if user !== domain cant access admin || editor

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,20 +1,35 @@
 import '@reshuffle/code-transform/macro';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { useAuth } from '@reshuffle/react-auth';
+import { checkEmail } from '../../backend/contentBackend';
 
 /**
  * Standard React-router route that requires  reshuffle identity
  */
 const PrivateRoute = ({ props, component: Component, ...rest }) => {
   const { authenticated } = useAuth();
+  const [email, setEmail] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (authenticated) {
+        const value = await checkEmail();
+        setEmail(value);
+      }
+    };
+
+    fetchData();
+    // eslint-disable-next-line
+  }, []);
+
   if (authenticated === undefined) return null;
 
   return (
     <Route
       {...rest}
       render={routeProps => {
-        if (authenticated) {
+        if (authenticated && email) {
           return <Component {...routeProps} {...props} />;
         } else {
           // if authenticated is false, redirect to auth page
