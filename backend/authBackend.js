2864c90302ded979b7998c41c2a9b123d9d29b88
josephriosio <blankworldwide@gmail.com>
2019-12-17 16:48:26 -0800

    linter fixes and removing duplicate code

--- backend/authBackend.js
+++ backend/authBackend.js
@@ -2,8 +2,16 @@ import { getCurrentUser } from '@reshuffle/server-function';
 const { REACT_APP_VALID_HOSTED_DOMAIN } = process.env;
 
 // validates if email is the right REACT_APP_VALID_HOSTED_DOMAIN
+/**
+ *
+ * @return {boolean} - returns if user is REACT_APP_VALID_HOSTED_DOMAIN matches their email
+ */
+/** @expose */
 export async function validateUser() {
-  const profile = getCurrentUser(true);
+  const profile = getCurrentUser(false);
+  if (profile === undefined) {
+    return { errors: { msg: 'User is not logged in.' } }
+  }
   let email = profile.emails[0].value;
   email = email.split('@');
 
@@ -11,20 +19,10 @@ export async function validateUser() {
     return true;
   }
 
-  throw new Error(
-    `User cannot be authenticated, ${email[1]} is not a valid host domain`,
-  );
-}
-
-// validates if email is the right REACT_APP_VALID_HOSTED_DOMAIN
-export async function validate() {
-  const profile = getCurrentUser(true);
-  let email = profile.emails[0].value;
-  email = email.split('@');
-
-  if (email[1] === REACT_APP_VALID_HOSTED_DOMAIN) {
-    return true;
-  }
-
-  return false;
+  return {
+    error: {
+      msg: `User cannot be authenticated, ${email[1]} is not a valid host domain,
+  valued host is ${REACT_APP_VALID_HOSTED_DOMAIN}.`,
+    },
+  };
 }
