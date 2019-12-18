4425e04d8c9e62756ff23e4c62771a46194c9967
josephriosio <blankworldwide@gmail.com>
2019-12-18 13:56:49 -0800

    removed throwerr function

--- backend/authBackend.js
+++ backend/authBackend.js
@@ -1,35 +1,54 @@
 import { getCurrentUser } from '@reshuffle/server-function';
-const { REACT_APP_VALID_HOSTED_DOMAIN } = process.env;
 
-//throw error once user is not a valid domain
-export async function throwErr() {
-  throw new Error(`User not valid, ${REACT_APP_VALID_HOSTED_DOMAIN} is correct domain.`)
+const { VALID_HOSTED_DOMAINS } = process.env;
+const domains = VALID_HOSTED_DOMAINS.split(' ');
+
+/**
+* validates if email is the right VALID_HOSTED_DOMAIN
+* setting getCurrentUser to false since I'm calling this function in the front end and I don't want
+* want the CRA to crash for not being logged in if user hits this function.
+/** @expose */
+export async function checkIfValidDomain() {
+  const profile = getCurrentUser(false);
+
+  if (profile === undefined) {
+    return false;
+  }
+
+  const emails = profile.emails.map((email) => {
+    return email.value
+  })
+
+  for (let i = 0; i < emails.length; i++) {
+    const email = emails[i]
+
+    for (let j = 0; j < domains.length; j++) {
+      const validDomain = domains[j]
+
+      if (email.includes(validDomain)) {
+        return true;
+      }
+    }
+  }
+
+  return false;
 }
 
-// validates if email is the right REACT_APP_VALID_HOSTED_DOMAIN
+
 /**
  *
- * @return {boolean} - returns if user is REACT_APP_VALID_HOSTED_DOMAIN matches their email
+ * @return {boolean} - returns if user is VALID_HOSTED_DOMAIN matches their email
  */
-/** @expose */
 export async function validateUser() {
-  const profile = getCurrentUser(false);
-  if (profile === undefined) {
-    return { errors: { msg: 'User is not logged in.' } }
-  }
-  let email = profile.emails[0].value;
-  email = email.split('@');
+  const profile = getCurrentUser(true);
 
-  if (email[1] === REACT_APP_VALID_HOSTED_DOMAIN) {
-    return true;
+  const email = profile.emails[0].value;
+
+  if (!checkIfValidDomain()) {
+    throw new Error(`User cannot be authenticated, ${email} is not a valid host domain.`)
   }
 
-  return {
-    error: {
-      msg: `User cannot be authenticated, ${email[1]} is not a valid host domain,
-  valued host is ${REACT_APP_VALID_HOSTED_DOMAIN}.`,
-    },
-  };
+  return true;
 }
 
 
