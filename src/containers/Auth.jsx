d85a2e1248ada3414a479ed1d148ecdd3edfb585
josephriosio <blankworldwide@gmail.com>
2019-12-19 12:03:42 -0800

    added lodash.get and implemented it

--- src/containers/Auth.jsx
+++ src/containers/Auth.jsx
@@ -1,14 +1,12 @@
 import '@reshuffle/code-transform/macro';
 import React from 'react';
 import { useAuth } from '@reshuffle/react-auth';
-
 import '../style/Auth.scss';
+import get from 'lodash.get';
+
 export default props => {
   const { getLoginURL } = useAuth();
-  let path = (props
-    && props.location
-    && props.location.state
-    && props.location.state.path) || 'editor';
+  const path = get(props, 'location.state.path', '/editor')
 
   return (
     <div className='auth'>
